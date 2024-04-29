3880d0f21ed1cd7e9222afbd00e3d9167135b28e
julioguirola <julioguirola03@gmail.com>
2024-04-28 20:28:55 -0400

    full interface

--- app/layout.js
+++ app/layout.js
@@ -9,24 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>
-          <div>
-            <div className="head-title">
-              <h2>AI ChatBot 🦾</h2>
-              <p>Hecho con amor por Julius 💙</p>
-            </div>
-            <div className="new-chat-button">
-              <button><svg  xmlns="http://www.w3.org/2000/svg"  width="40"  height="40"  viewBox="0 0 24 24"  fill="none"  stroke="white"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-square-rounded-plus"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9 -9 9s-9 -1.8 -9 -9s1.8 -9 9 -9z" /><path d="M15 12h-6" /><path d="M12 9v6" /></svg></button>
-              <p>New Chat</p>
-            </div>
-          </div>
-        </header>
-        <main>
           {children}
-        </main>
-        <footer>
-          <input type="text" placeholder="Ask something ..." id="textPrompt"></input>
-        </footer>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -3,6 +3,16 @@ import { useState, useEffect } from "react";
 import Markdown from "react-markdown"
 import styles from "./page.module.css";
 
+function Msg () {
+  return (
+    <>
+      <article>
+        <h4>🔵 Model :</h4>
+        <p>lorem ipsum ohhhhhasdsadasdsadasda  sdfdsf sdfdsf sdfsdf sdfsd sdf     dhnfghnfghnghn</p>
+      </article>
+    </>
+  )
+}
 
 export default function Home() {
   const [history, setHistory] = useState([])
@@ -11,6 +21,49 @@ export default function Home() {
 
   return (
     <>
+    <header>
+          <div>
+            <div className="head-title">
+              <h2>AI ChatBot 🦾</h2>
+              <p>Hecho con amor por Julius 💙</p>
+            </div>
+            <div className="new-chat-button">
+              <button><svg xmlns="http://www.w3.org/2000/svg" width={40} height={40} viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-square-plus">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 12h6" />
+                <path d="M12 9v6" />
+                <path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z" />
+              </svg>
+              </button>
+              <p>New Chat</p>
+            </div>
+          </div>
+        </header>
+        <main>
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+          <Msg />
+        </main>
+        <footer>
+          <div className="search">
+            <input placeholder="Ask something ..." type="text"></input>
+          <button>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 664 663" height="15" width="15">
+              <path fill="none" d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"></path>
+              <path strokeLinejoin="round" strokeLinecap="round" strokeWidth="33.67" stroke="white" d="M646.293 331.888L17.7538 17.6187L155.245 331.888M646.293 331.888L17.753 646.157L155.245 331.888M646.293 331.888L318.735 330.228L155.245 331.888"></path>
+            </svg>
+          </button>
+          </div>
+        </footer>
     </>
   )
 }
