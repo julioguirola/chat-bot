a2f78db51f5821bd7acff890dde1f7e2fe725283
julioguirola <julioguirola03@gmail.com>
2024-07-10 15:37:16 -0400

    msg ready2

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/ask",
+                source: "/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
